a56a0b9bb34dffb25a36bc9793a67e3ea544e6d5
sagar23sj <sagarsonwane23@gmail.com>
2022-07-20 12:02:44 +0530

    addeed greeting package

---
+++ greetings.go
@@ -0,0 +1,13 @@
+package greeting
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func Hello(name string) string {
+	color.Cyan("welcome %s", name)
+	msg := fmt.Sprint("Hi, %s. Welcome!", name)
+	return msg
+}
